a2f7bd6800f3683e10c8320634420f7f46173531
Milan Pathak <milanpathak73@gmail.com>
2024-08-28 18:57:07 +0530

    port change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // app.get('/Courtyard-Bowling-Green-Convention-Center', async (req, res) => {
 
